003f19eeebd1dd4d44fc7e034cd58ea4a8c1d792
chestlyace <derryacceey@gmail.com>
2026-07-15 07:38:24 +0100

    fix(mobile): correct jest.config.js setupFiles key

--- mobile/jest.config.js
+++ mobile/jest.config.js
@@ -3,7 +3,7 @@ module.exports = {
   transformIgnorePatterns: [
     "node_modules/(?!((jest-)?react-native|@react-native(-community)?)|expo(nent)?|@expo(nent)?/.*|@expo-google-fonts/.*|react-navigation|@react-navigation/.*|@sentry/react-native|native-base|react-native-svg|react-native-chart-kit|nativewind|tailwindcss)",
   ],
-  setupFilesAfterSetup: ["@testing-library/jest-native/extend-expect"],
+  setupFiles: ["@testing-library/jest-native/extend-expect"],
   moduleFileExtensions: ["ts", "tsx", "js", "jsx", "json"],
   moduleNameMapper: {
     "^@/(.*)$": "<rootDir>/$1",
